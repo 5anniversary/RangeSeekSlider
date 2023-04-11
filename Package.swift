6450850057b8a5f75c7960db5511af4c72cdab50
junhyeon.oh <oh.junhyeon95@gmail.com>
2023-04-11 22:18:24 +0900

    spm with swift 5.8

--- Package.swift
+++ Package.swift
@@ -1,5 +1,15 @@
-import PackageDescription
+// swift-tools-version:5.8
+ import PackageDescription
 
-let package = Package(
-    name: "RangeSeekSlider"
-)
+ let package = Package(
+     name: "RangeSeekSlider",
+     products: [
+         .library(name: "RangeSeekSlider", targets: ["RangeSeekSlider"])
+     ],
+     targets: [
+         .target(
+             name: "RangeSeekSlider",
+             dependencies: []
+         )
+     ]
+ )
